d15f638acfc1aac6146d6a32e11bced3a89ffb6c
David Skoda <dskoda1@binghamton.edu>
2017-02-12 17:54:02 -0500

    add test for assignRouteToHttp

--- gopi_test.go
+++ gopi_test.go
@@ -3,6 +3,9 @@ package gopi
 import (
   "testing"
   "net/http"
+  "net/http/httptest"
+
+  "github.com/stretchr/testify/assert"
 )
 
 // Handler to demonstrate testing of request handlers
@@ -17,4 +20,21 @@ func Test_handler(t *testing.T) {
   HandlerTestUtil(t, &HandlerTestParams{
     "GET", "/", nil, handler_test, http.StatusOK, "sweet",
   })
+}
+
+func TestAssignRouteToHttp(t *testing.T) {
+  router := NewRouter()
+  handler_func_wrapper := router.assignRouteToHttp(handler_test)
+
+  // Create a recorder to observe changes to later.
+  rr := httptest.NewRecorder()
+  // Create the request provided fields from the HandlerUtil.
+  req, err := http.NewRequest("GET", "/", nil)
+  assert.Nil(t, err)
+
+  handler_func_wrapper(rr, req)
+
+  assert.Equal(t, http.StatusOK, rr.Code)
+  assert.Equal(t, "sweet", rr.Body.String())
+
 }
